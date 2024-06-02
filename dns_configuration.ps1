50aa15de5ffc565a535847c621d5710b9e6a19d5
Landry Gonzalez <161647468+landrygonzalez@users.noreply.github.com>
2024-06-02 22:15:20 +0200

    Update and rename dns_configuration to dns_configuration.ps1

--- dns_configuration.ps1
+++ dns_configuration.ps1
@@ -1,6 +1,6 @@
 # Déclaration des variables.
-$dns_primaire = 9.9.9.9
-$dns_secondaire = 149.112.112.112
+$dns_primaire = "9.9.9.9"
+$dns_secondaire = "149.112.112.112"
 # Les DNS ci-dessus sont ceux de Quad9 : https://quad9.net/fr/service/service-addresses-and-features
 
 # Récupérer les index des interfaces UP.
@@ -16,3 +16,8 @@ foreach ($i in $interfaces_up){
 
 $interface_selectionnee = Read-host "Saisissez maintenant le numéro de l’interface active dont vous voulez configurer les DNS "
 Set-DnsClientServerAddress -InterfaceIndex $interface_selectionnee -ServerAddresses ($dns_primaire,$dns_secondaire)
+
+Write-host "Les paramètres DNS IPv4 pour l'interface sélectionnée sont définis comme suit : "
+Get-DnsClientServerAddress -InterfaceIndex $interface_selectionnee | Where-Object AddressFamily -Contains "2" | Select-Object InterfaceAlias,ServerAddresses
+
+Read-Host -Prompt "Press Enter to continue..."
